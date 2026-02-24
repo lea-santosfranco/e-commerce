aff692f0d3fb34d651dde36641572a38b5e872e6
Léa SANTOS FRANCO <lea.santosfrancopro@gmail.com>
2026-02-24 14:51:21 +0100

    maj homepage

---
+++ src/Controller/HomePageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomePageController extends AbstractController
+{
+    #[Route('/', name: 'app_home_page')]
+    public function index(): Response
+    {
+        $StudentsNames = ['Alice', 'Bob', 'Charlie', 'David', 'Eve'];
+        $StudentsAges = [20, 22, 19, 21, 23];
+        return $this->render('home_page/index.html.twig', [
+            'controller_name' => 'HomePageController',
+        ]);
+    }
+}

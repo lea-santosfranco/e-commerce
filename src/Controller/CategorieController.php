b8dcb46de159b882ca3740347f3034db4ab2140f
Léa SANTOS FRANCO <lea.santosfrancopro@gmail.com>
2026-02-25 14:05:15 +0100

    Ajout categorie form type

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class CategorieController extends AbstractController
 {
@@ -12,6 +13,13 @@ final class CategorieController extends AbstractController
     public function index(): Response
     {
         return $this->render('categorie/index.html.twig', [
+            'controller_name' => 'CategorieController',
+        ]);
+    }
+    #[Route('/categorie/new', name: 'app_categorie_new')]
+    public function addCategorie(EntityManagerInterface $entityManager): Response
+    {
+        return $this->render('categorie/newCategorie.html.twig', [
             'controller_name' => 'CategorieController',
         ]);
     }

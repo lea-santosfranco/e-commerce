74d3256772366b81c384fde1410e73cf12a0edae
Léa SANTOS FRANCO <lea.santosfrancopro@gmail.com>
2026-02-25 13:39:36 +0100

    Exercice 1 Symfony création de l'entity catégorie, migration et création du Categorie Controller

---
+++ migrations/Version20260225123446.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260225123446 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE categorie CHANGE name name VARCHAR(180) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_497DD6345E237E06 ON categorie (name)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_497DD6345E237E06 ON categorie');
+        $this->addSql('ALTER TABLE categorie CHANGE name name VARCHAR(255) NOT NULL');
+    }
+}

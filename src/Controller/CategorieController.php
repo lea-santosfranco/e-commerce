396407b2f8641581af17cb263879383b5def9b53
Léa SANTOS FRANCO <lea.santosfrancopro@gmail.com>
2026-02-26 09:58:10 +0100

    maj exercice 2

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -25,14 +25,31 @@ final class CategorieController extends AbstractController
         $category = new Categorie();
 
         $form = $this->createForm(CategorieFormType::class, $category);
-            $form->handleRequest($request);
+        $form->handleRequest($request);
 
-            if ($form->isSubmitted() && $form->isValid()) {
-                $entityManager->persist($category);
-                $entityManager->flush();
-            }
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($category);
+            $entityManager->flush();
+        }
 
         return $this->render('categorie/newCategorie.html.twig', [
+            // 'controller_name' => 'CategorieController',
+            "form" => $form->createView()
+        ]);
+    }
+
+    #[Route('/categorie/update/{id}', name: 'app_categorie_update')]
+    public function updateCategorie(EntityManagerInterface $entityManager, Request $request, Categorie $categorie): Response
+    {
+        $form = $this->createForm(CategorieFormType::class, $categorie);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+                return $this->redirectToRoute('app_categorie');
+        }
+
+        return $this->render('categorie/updateCategorie.html.twig', [
             // 'controller_name' => 'CategorieController',
             "form" => $form->createView()
         ]);

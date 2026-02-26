98aca39802da7e8c5f8dfa4c00fe1372b4bb1760
Léa SANTOS FRANCO <lea.santosfrancopro@gmail.com>
2026-02-26 16:08:37 +0100

    message: Update UserController to fetch users from the database and display them in the index view.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Attribute\Route;
 final class UserController extends AbstractController
 {
     #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
+
+        $users = $userRepository -> findAll();
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            // 'controller_name' => 'UserController',
+            'users' => $users
         ]);
     }
 }
